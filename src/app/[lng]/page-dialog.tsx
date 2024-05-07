3654834b4d65fa5c3bd0ba918f34fd717e2ae023
Felix Erdmann <hello@felixerdmann.com>
2024-05-07 13:37:02 +0200

    feat: add partner logos

--- src/app/[lng]/page-dialog.tsx
+++ src/app/[lng]/page-dialog.tsx
@@ -1,5 +1,3 @@
-import { promises as fs } from "fs";
-import path from "path";
 import { BackgroundBeams } from "@/components/animated/beams";
 import { GlowingStarsBackgroundCard } from "@/components/animated/glowing-stars";
 import { useMDXComponents } from "@/components/mdx/mdx-components";
@@ -7,103 +5,105 @@ import { Card } from "@/components/ui/card";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
+import { promises as fs } from "fs";
 import { compileMDX } from "next-mdx-remote/rsc";
 import Image from "next/image";
+import path from "path";
 import { Suspense } from "react";
 
 async function getData(content: string, lng = "de") {
-	try {
-		const file = await fs.readFile(
-			path.join(
-				process.cwd(),
-				"src",
-				"content",
-				content,
-				`${content}.${lng}.mdx`,
-			),
-			"utf8",
-		);
-		if (!file) return { mdxSource: "" };
-		return { mdxSource: file };
-	} catch (error) {
-		return { mdxSource: "" };
-	}
+  try {
+    const file = await fs.readFile(
+      path.join(
+        process.cwd(),
+        "src",
+        "content",
+        content,
+        `${content}.${lng}.mdx`
+      ),
+      "utf8"
+    );
+    if (!file) return { mdxSource: "" };
+    return { mdxSource: file };
+  } catch (error) {
+    return { mdxSource: "" };
+  }
 }
 
 export default async function PageDialog({
-	content,
-	lng,
-	animatedBackground,
-	children,
-	className,
-	image,
+  content,
+  lng,
+  animatedBackground,
+  children,
+  className,
+  image,
 }: {
-	content: string;
-	lng: string;
-	animatedBackground?:
-		| "beams"
-		| "glowing-stars"
-		| "grid"
-		| "background-gradient";
-	children: React.ReactNode;
-	className?: string;
-	image?: string;
+  content: string;
+  lng: string;
+  animatedBackground?:
+    | "beams"
+    | "glowing-stars"
+    | "grid"
+    | "background-gradient";
+  children: React.ReactNode;
+  className?: string;
+  image?: string;
 }) {
-	const { mdxSource } = await getData(content, lng);
+  const { mdxSource } = await getData(content, lng);
 
-	const { content: mdxContent, frontmatter } = await compileMDX<{
-		title: string;
-	}>({
-		source: mdxSource,
-		options: { parseFrontmatter: true },
-		components: useMDXComponents({}),
-	});
+  const { content: mdxContent, frontmatter } = await compileMDX<{
+    title: string;
+  }>({
+    source: mdxSource,
+    options: { parseFrontmatter: true },
+    components: useMDXComponents({}),
+  });
 
-	const Trigger = () => (
-		<Card
-			className={cn(
-				"from-muted to-background/90",
-				"bg-gradient-to-br w-full break-inside-avoid relative rounded-md overflow-hidden group cursor-pointer",
-				className,
-			)}
-		>
-			{animatedBackground === "beams" && (
-				<BackgroundBeams className="group-hover:scale-105 transition-all" />
-			)}
-			{animatedBackground === "glowing-stars" && (
-				<GlowingStarsBackgroundCard className="group-hover:scale-105 transition-all" />
-			)}
-			{animatedBackground === "grid" && (
-				<div className="absolute top-0 left-0 w-full h-full group-hover:scale-105 transition-all">
-					<div className="h-full w-full bg-transparent  bg-grid-white/[0.2] relative flex items-center justify-center">
-						<div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-					</div>
-				</div>
-			)}
-			{image && (
-				<Image
-					src={image}
-					fill
-					objectFit="cover"
-					className="object-center"
-					alt={content}
-				/>
-			)}
+  const Trigger = () => (
+    <Card
+      className={cn(
+        "from-muted to-background/90",
+        "bg-gradient-to-br w-full break-inside-avoid relative rounded-md overflow-hidden group cursor-pointer",
+        className
+      )}
+    >
+      {animatedBackground === "beams" && (
+        <BackgroundBeams className="group-hover:scale-105 transition-all" />
+      )}
+      {animatedBackground === "glowing-stars" && (
+        <GlowingStarsBackgroundCard className="group-hover:scale-105 transition-all" />
+      )}
+      {animatedBackground === "grid" && (
+        <div className="absolute top-0 left-0 w-full h-full group-hover:scale-105 transition-all">
+          <div className="h-full w-full bg-transparent  bg-grid-white/[0.2] relative flex items-center justify-center">
+            <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+          </div>
+        </div>
+      )}
+      {image && (
+        <Image
+          src={image}
+          fill
+          objectFit="cover"
+          className="object-center brightness-50"
+          alt={content}
+        />
+      )}
 
-			<div className="absolute top-0 left-0 w-full h-full p-4">{children}</div>
-		</Card>
-	);
+      <div className="absolute top-0 left-0 w-full h-full p-4">{children}</div>
+    </Card>
+  );
 
-	return (
-		<Dialog>
-			<DialogTrigger asChild>
-				<Trigger />
-			</DialogTrigger>
-			<DialogContent className="max-w-5xl rounded-lg overflow-auto no-scrollbar">
-				<ScrollArea className="max-h-[80vh] p-4">
-					<Suspense fallback={"Loading..."}>{mdxContent}</Suspense>
-				</ScrollArea>
-			</DialogContent>
-		</Dialog>
-	);
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Trigger />
+      </DialogTrigger>
+      <DialogContent className="max-w-5xl rounded-lg overflow-auto no-scrollbar">
+        <ScrollArea className="max-h-[80vh] p-4">
+          <Suspense fallback={"Loading..."}>{mdxContent}</Suspense>
+        </ScrollArea>
+      </DialogContent>
+    </Dialog>
+  );
 }
--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -5,114 +5,123 @@ import { redirect } from "next/navigation";
 import PageDialog from "./page-dialog";
 
 export default async function Page({
-	params,
+  params,
 }: {
-	params: { lng: "de" | "en" | "pt" };
+  params: { lng: "de" | "en" | "pt" };
 }) {
-	if (!params.lng || !["de", "en", "pt"].includes(params.lng)) redirect("/de");
+  if (!params.lng || !["de", "en", "pt"].includes(params.lng)) redirect("/de");
 
-	return (
-		<div className="flex flex-col items-center w-full gap-12">
-			<div className="max-w-6xl mx-auto w-full">
-				<div className="grid lg:auto-rows-[16rem] md:auto-rows-[14rem] grid-cols-2 md:grid-cols-4 gap-4 auto-rows-[12rem]">
-					<div className="h-full w-full col-span-2 bg-dot-fuchsia-400/50 relative flex flex-col items-start justify-center px-4 md:px-8">
-						<div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_45%,black)]" />
-						<H1 className="bg-clip-text text-transparent bg-gradient-to-b from-muted-foreground to-foreground py-0">
-							Atrai Bikes
-						</H1>
-						<H4>
-							transforms bike commuting by providing real-time data insights,
-							fostering collaboration for cyclist-friendly cities.
-						</H4>
-					</div>
-					<PageDialog
-						lng={params.lng}
-						content="project"
-						animatedBackground="beams"
-					>
-						<div className="absolute bottom-0 left-0 p-4">
-							<h2 className="text-2xl font-bold">{i18n[params.lng].project}</h2>
-						</div>
-					</PageDialog>
+  return (
+    <div className="flex flex-col items-center w-full gap-12">
+      <div className="max-w-6xl mx-auto w-full">
+        <div className="grid lg:auto-rows-[16rem] md:auto-rows-[14rem] grid-cols-2 md:grid-cols-4 gap-4 auto-rows-[12rem]">
+          <div className="h-full w-full col-span-2 bg-dot-fuchsia-400/50 relative flex flex-col items-start justify-center px-4 md:px-8">
+            <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_45%,black)]" />
+            <H1 className="bg-clip-text text-transparent bg-gradient-to-b from-muted-foreground to-foreground py-0">
+              Atrai Bikes
+            </H1>
+            <H4>
+              transforms bike commuting by providing real-time data insights,
+              fostering collaboration for cyclist-friendly cities.
+            </H4>
+          </div>
+          <PageDialog
+            lng={params.lng}
+            content="project"
+            animatedBackground="beams"
+          >
+            <div className="absolute bottom-0 left-0 p-4">
+              <h2 className="text-2xl font-bold">{i18n[params.lng].project}</h2>
+            </div>
+          </PageDialog>
 
-					<PageDialog
-						lng={params.lng}
-						content="news"
-						animatedBackground="glowing-stars"
-					>
-						<div className="absolute bottom-0 left-0 p-4">
-							<h2 className="text-2xl font-bold">{i18n[params.lng].news}</h2>
-						</div>
-					</PageDialog>
+          <PageDialog
+            lng={params.lng}
+            content="news"
+            animatedBackground="glowing-stars"
+          >
+            <div className="absolute bottom-0 left-0 p-4">
+              <h2 className="text-2xl font-bold">{i18n[params.lng].news}</h2>
+            </div>
+          </PageDialog>
 
-					<PageDialog
-						lng={params.lng}
-						content="product"
-						className="col-span-2 md:col-span-3"
-						image="https://cloud.reedu.de/s/zRccjmGC5c3HB89/download"
-					>
-						<div className="absolute bottom-0 left-0 p-4">
-							<h2 className="text-2xl font-bold">{i18n[params.lng].product}</h2>
-						</div>
-					</PageDialog>
+          <PageDialog
+            lng={params.lng}
+            content="product"
+            className="col-span-2 md:col-span-3"
+            image="https://cloud.reedu.de/s/zRccjmGC5c3HB89/download"
+          >
+            <div className="absolute bottom-0 left-0 p-4">
+              <h2 className="text-2xl font-bold">{i18n[params.lng].product}</h2>
+            </div>
+          </PageDialog>
 
-					<PageDialog
-						lng={params.lng}
-						content="connect"
-						className="col-span-2 md:col-span-1"
-					>
-						<div className="absolute bottom-0 left-0 p-4">
-							<h2 className="text-2xl font-bold">{i18n[params.lng].connect}</h2>
-						</div>
-					</PageDialog>
+          <PageDialog
+            lng={params.lng}
+            content="connect"
+            className="col-span-2 md:col-span-1"
+          >
+            <div className="absolute bottom-0 left-0 p-4">
+              <h2 className="text-2xl font-bold">{i18n[params.lng].connect}</h2>
+            </div>
+          </PageDialog>
 
-					<PageDialog
-						lng={params.lng}
-						content="documents"
-						animatedBackground="grid"
-					>
-						<div className="absolute bottom-0 left-0 p-4">
-							<h2 className="text-2xl font-bold">
-								{i18n[params.lng].documents}
-							</h2>
-						</div>
-					</PageDialog>
+          <PageDialog
+            lng={params.lng}
+            content="documents"
+            animatedBackground="grid"
+          >
+            <div className="absolute bottom-0 left-0 p-4">
+              <h2 className="text-2xl font-bold">
+                {i18n[params.lng].documents}
+              </h2>
+            </div>
+          </PageDialog>
 
-					<PageDialog
-						lng={params.lng}
-						content="faq"
-						animatedBackground="glowing-stars"
-					>
-						<div className="absolute bottom-0 left-0 p-4">
-							<h2 className="text-2xl font-bold">{i18n[params.lng].faq}</h2>
-						</div>
-					</PageDialog>
+          <PageDialog
+            lng={params.lng}
+            content="faq"
+            animatedBackground="glowing-stars"
+          >
+            <div className="absolute bottom-0 left-0 p-4">
+              <h2 className="text-2xl font-bold">{i18n[params.lng].faq}</h2>
+            </div>
+          </PageDialog>
 
-					<PageDialog
-						lng={params.lng}
-						content="insights"
-						image="https://cloud.reedu.de/s/ZePFfYBETGWDNmn/download"
-						className="col-span-2 md:row-span-2"
-					>
-						<div className="absolute bottom-0 left-0 p-4">
-							<h2 className="text-2xl font-bold">
-								{i18n[params.lng].insights}
-							</h2>
-						</div>
-					</PageDialog>
+          <PageDialog
+            lng={params.lng}
+            content="insights"
+            image="https://cloud.reedu.de/s/ZePFfYBETGWDNmn/download"
+            className="col-span-2 md:row-span-2"
+          >
+            <div className="absolute bottom-0 right-0 p-4">
+              <h2 className="text-2xl font-bold">
+                {i18n[params.lng].insights}
+              </h2>
+            </div>
+          </PageDialog>
 
-					<PageDialog
-						lng={params.lng}
-						content="partner"
-						animatedBackground="beams"
-						className="col-span-2"
-					>
-						<div className="absolute bottom-0 left-0 p-4">
-							<h2 className="text-2xl font-bold">{i18n[params.lng].partner}</h2>
-						</div>
-					</PageDialog>
-				</div>
-			</div>
-		</div>
-	);
+          <PageDialog lng={params.lng} content="partner" className="col-span-2">
+            <div className="absolute top-0 left-0 w-full h-full flex items-center justify-between py-10 md:py-16">
+              <img
+                src={"https://cloud.reedu.de/s/BSt4PwxEqBiK9fS/download"}
+                className="h-full w-auto"
+              />
+              <img
+                src={"https://cloud.reedu.de/s/mYk9oe45eKFCW6a/download"}
+                className="h-full w-auto"
+              />
+              <img
+                src={"https://cloud.reedu.de/s/D8KoCXaqQibF3kL/download"}
+                className="h-full w-auto"
+              />
+            </div>
+            <div className="absolute bottom-0 left-0 p-4">
+              <h2 className="text-2xl font-bold">{i18n[params.lng].partner}</h2>
+            </div>
+          </PageDialog>
+        </div>
+      </div>
+    </div>
+  );
 }
