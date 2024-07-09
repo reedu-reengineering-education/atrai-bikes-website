74ea845c17839dc6925963c5214600c481599870
Felix Erdmann <hello@felixerdmann.com>
2024-07-09 09:20:57 +0200

    feat: update footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,19 +1,28 @@
 export default function Footer() {
   return (
-    <footer className="w-full bg-white text-background py-6 md:py-12 text-center">
+    <footer className="w-full bg-white text-background p-2 md:p-6 text-center flex items-center justify-between text-sm flex-col md:flex-row">
       <p>© {new Date().getFullYear()} Atrai Bikes</p>
-      <div className="flex justify-center items-center gap-12 mt-4 flex-wrap">
+      <div className="flex justify-center items-center gap-6 mt-2 flex-wrap">
         <img
           src={"https://cloud.reedu.de/s/kgCBLmrHENwx2HS/download"}
-          className="w-60 h-auto"
+          className="w-40 h-auto"
           alt={""}
         />
         <img
           src={"https://cloud.reedu.de/s/ykrJWwjbYTNE4nD/download"}
-          className="w-auto h-20"
+          className="w-auto h-10"
           alt={""}
         />
       </div>
+      <p className="text-left">
+        Reedu GmbH & Co. KG
+        <br />
+        Von-Steuben-Str. 21 - 48143 Münster
+        <br />
+        +49 251 98119797
+        <br />
+        kontakt@reedu.de
+      </p>
     </footer>
   );
 }
