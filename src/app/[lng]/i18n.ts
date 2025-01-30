72dec6bf3e3633c5da4586fd35ba1a528cf25a01
akimshum <arturshum@gmail.com>
2025-01-30 19:08:19 +0100

    Portuguese version
    PT version is updated and the slogan in Portuguese and DT is updated as well.

--- src/app/[lng]/i18n.ts
+++ src/app/[lng]/i18n.ts
@@ -9,7 +9,9 @@ export const i18n = {
 		insights: "Einblicke",
 		partner: "Partner",
 		announcement: "Nehmen Sie an unserem Workshop am 30. Oktober 2024 in Münster teil.",
-		announcementSubtitle: "Mit der senseBox:bike zur Data-driven Fahrradstadt"
+		announcementSubtitle: "Mit der senseBox:bike zur Data-driven Fahrradstadt", 
+		projectSubtitle: "Transformiert das Fahrradpendeln, indem es Echtzeit-Daten bereitstellt und die Zusammenarbeit für fahrradfreundliche Städte fördert."
+
 	},
 	en: {
 		project: "Project",
@@ -21,7 +23,8 @@ export const i18n = {
 		insights: "Insights",
 		partner: "Partners",
 		announcement: "Join our workshop on the 30th of October, 2024 in Münster.",
-		announcementSubtitle: "With the senseBox:bike to the data-driven bicycle city"
+		announcementSubtitle: "With the senseBox:bike to the data-driven bicycle city",
+		projectSubtitle: "Transforms bike commuting by providing real-time data insights, fostering collaboration for cyclist-friendly cities."
 	},
 	pt: {
 		project: "Projeto",
@@ -33,6 +36,6 @@ export const i18n = {
 		insights: "Insight",
 		partner: "Parceiros",
 		announcement: "Junte-se ao nosso workshop no dia 30 de outubro de 2024 em Münster.",
-		announcementSubtitle: "Com a senseBox:bike para a cidade de bicicletas orientada por dados"
+		projectSubtitle: "Transforma o deslocamento de bicicleta fornecendo informações sobre as condições urbanas e colaborando para cidades para ciclistas"
 	},
 };
