9a314edc370400ee715cbd74787133d6386ca50f
Felix Erdmann <hello@felixerdmann.com>
2025-04-01 11:41:43 +0200

    fix: formatting

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,15 +1,9 @@
-import { BackgroundBeams } from "@/components/animated/beams";
-import { GlowingStarsBackgroundCard } from "@/components/animated/glowing-stars";
 import { useMDXComponents } from "@/components/mdx/mdx-components";
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
 import { promises as fs } from "fs";
 import * as LucideIcons from "lucide-react";
 import { compileMDX } from "next-mdx-remote/rsc";
-import Image from "next/image";
 import Link from "next/link";
 import path from "path";
 import { Suspense } from "react";
@@ -18,22 +12,9 @@ import remarkGfm from "remark-gfm";
 async function getData() {
   try {
     const file = await fs.readFile(
-      path.join(
-        process.cwd(),
-        "src",
-        "content",
-        "docs",
-        `docs.en.mdx`
-      ),
+      path.join(process.cwd(), "src", "content", "docs", `docs.en.mdx`),
       "utf8"
     );
-    console.log("file", path.join(
-      process.cwd(),
-      "src",
-      "content",
-      "docs",
-      `docs.en.mdx`
-    ));
     if (!file) return { mdxSource: "" };
     return { mdxSource: file };
   } catch (error) {
@@ -62,14 +43,11 @@ export default async function Page() {
       Button: Button,
       Link: Link,
       table: (props: any) => (
-          <table
-            className={cn(
-              "w-full border-collapse text-sm",
-              props.className
-            )}
-          >
-            {props.children}
-          </table>
+        <table
+          className={cn("w-full border-collapse text-sm", props.className)}
+        >
+          {props.children}
+        </table>
       ),
       th: (props: any) => (
         <th
@@ -82,12 +60,7 @@ export default async function Page() {
         </th>
       ),
       td: (props: any) => (
-        <td
-          className={cn(
-            "border-b px-4 py-2 text-left",
-            props.className
-          )}
-        >
+        <td className={cn("border-b px-4 py-2 text-left", props.className)}>
           {props.children}
         </td>
       ),
@@ -106,7 +79,5 @@ export default async function Page() {
     }),
   });
 
-  return (
-          <Suspense fallback={"Loading..."}>{mdxContent}</Suspense>
-  );
+  return <Suspense fallback={"Loading..."}>{mdxContent}</Suspense>;
 }
